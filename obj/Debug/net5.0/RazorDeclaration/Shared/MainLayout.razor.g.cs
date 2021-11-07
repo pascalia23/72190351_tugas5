b4f6d1cf9d21cf4516e1c2616e294cc08252a5ab
Pascalia23 <pascalia.girsang@si.ukdw.ac.id>
2021-11-07 22:43:23 +0700

    first commit

--- obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace Blazor344.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\72190344_Tugas5\Blazor\Blazor344\_Imports.razor"
+#line 1 "C:\Front\GBU\Blazor\Blazor344\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\72190344_Tugas5\Blazor\Blazor344\_Imports.razor"
+#line 2 "C:\Front\GBU\Blazor\Blazor344\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\72190344_Tugas5\Blazor\Blazor344\_Imports.razor"
+#line 3 "C:\Front\GBU\Blazor\Blazor344\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\72190344_Tugas5\Blazor\Blazor344\_Imports.razor"
+#line 4 "C:\Front\GBU\Blazor\Blazor344\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\72190344_Tugas5\Blazor\Blazor344\_Imports.razor"
+#line 5 "C:\Front\GBU\Blazor\Blazor344\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\72190344_Tugas5\Blazor\Blazor344\_Imports.razor"
+#line 6 "C:\Front\GBU\Blazor\Blazor344\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\72190344_Tugas5\Blazor\Blazor344\_Imports.razor"
+#line 7 "C:\Front\GBU\Blazor\Blazor344\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\72190344_Tugas5\Blazor\Blazor344\_Imports.razor"
+#line 8 "C:\Front\GBU\Blazor\Blazor344\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\72190344_Tugas5\Blazor\Blazor344\_Imports.razor"
+#line 9 "C:\Front\GBU\Blazor\Blazor344\_Imports.razor"
 using Blazor344;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\72190344_Tugas5\Blazor\Blazor344\_Imports.razor"
+#line 10 "C:\Front\GBU\Blazor\Blazor344\_Imports.razor"
 using Blazor344.Shared;
 
 #line default
